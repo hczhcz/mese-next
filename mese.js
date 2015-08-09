df7aceaf82575c0e2fa7a5b6d82172d8eb8e3ec9
hcz <h.cz@qq.com>
2015-08-10 00:29:26 +0800

    working

--- mese.js
+++ mese.js
@@ -13,29 +13,45 @@ var server = http.createServer(function (req, res) {
     var d = domain.create();
 
     d.on('error', function (e) {
-        // ignore
+        console.log(e);
     });
 
     d.add(req);
     d.add(res);
 
     d.run(function () {
+        console.log('web ' + req.connection.remoteAddress);
+
         res.writeHead(200);
         res.end(page);
     });
 }).listen(port);
 
 io(server).on('connection', function (socket) {
+    console.log('socket ' + socket.conn.remoteAddress);
+
     var d = domain.create();
 
     d.on('error', function (e) {
-        // ignore
+        console.log(e);
     });
 
     d.add(socket);
 
     d.run(function () {
         socket.on('login', function (data) {
+            if (typeof data.name != 'string') {
+                return;
+            }
+            if (typeof data.password != 'string') {
+                return;
+            }
+            if (!/^[A-Za-z0-9_ ]+$/i.test(name)) {
+                return;
+            }
+
+            console.log('login ' + data.name);
+
             socket.on('submit', function (data) {
                 //
             });

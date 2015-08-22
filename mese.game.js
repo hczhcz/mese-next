a763ec608bdc7226f7678e71845eafeba241fa21
hcz <h.cz@qq.com>
2015-08-22 17:01:16 +0800

    add function game.action

--- mese.game.js
+++ mese.game.js
@@ -1,7 +1,92 @@
 'use strict';
 
+var domain = require('domain');
+
 var core = require('./mese.core');
 var db = require('./mese.db');
+
+module.exports.action = function (storage) {
+    if (storage.dynamicGet('active')) {
+        return;
+    }
+
+    var d = domain.create();
+
+    d.on('error', function (e) {
+        storage.dynamicSet('active', false);
+
+        throw e;
+    });
+
+    d.add(storage);
+
+    d.run(function () {
+        if (storage.dynamicGet('active')) {
+            return;
+        }
+
+        storage.dynamicSet('active', true);
+
+        var handleDecision = function (gameData) {
+            var decisions = storage.dynamicGet('decisions');
+
+            if (decisions.length > 0) {
+                // submit
+
+                var decision = decisions.shift();
+
+                core.submit(
+                    gameData, decision.player,
+                    decision.price,
+                    decision.prod,
+                    decision.mk,
+                    decision.ci,
+                    decision.rd,
+                    function (gameData) {
+                        // accepted
+                        decision.callback(gameData);
+                        handleDecision(gameData);
+                    },
+                    function (gameData) {
+                        // declined
+                        decision.fail(gameData);
+                        handleDecision(gameData);
+                    }
+                );
+            } else {
+                // close
+
+                core.close(
+                    gameData,
+                    function (gameData) {
+                        // closed
+                        storage.staticSet(
+                            'data',
+                            gameData,
+                            function (doc) {
+                                storage.dynamicSet('active', false);
+                            }
+                        );
+                    },
+                    function (gameData) {
+                        // not closed
+                        storage.staticSet(
+                            'data',
+                            gameData,
+                            function (doc) {
+                                storage.dynamicSet('active', false);
+                            }
+                        );
+                    }
+                );
+            }
+        };
+
+        storage.staticGet('data', function (data) {
+            handleDecision(data.buffer);
+        });
+    });
+};
 
 module.exports.submit = function (
     storage, player,
@@ -26,6 +111,8 @@ module.exports.submit = function (
     });
 
     storage.dynamicSet('decisions', decisions);
+
+    module.exports.action(storage);
 };
 
 // TODO: handle 'decisions' buffer, close period, execute callbacks

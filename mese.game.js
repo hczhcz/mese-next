3a3b44eb85f47531471850e0aa6dbad7e2e02683
hcz <h.cz@qq.com>
2015-10-21 18:06:55 +0800

    add functions in mese.game.js

--- mese.game.js
+++ mese.game.js
@@ -5,6 +5,82 @@ var domain = require('domain');
 var core = require('./mese.core');
 var db = require('./mese.db');
 
+var addDecision = function (storage, decision) {
+    var decisions = storage.dynamicGet('decisions');
+
+    if (typeof decisions != 'array') {
+        decisions = [];
+        storage.dynamicSet('decisions', decisions);
+    }
+
+    // insert into queue
+    decisions.push(decision);
+};
+
+var handleDecisions = function (storage, gameData, callback, fail) {
+    var decisions = storage.dynamicGet('decisions');
+
+    if (decisions.length > 0) {
+        // submit
+
+        // get decision from queue
+        var decision = decisions.shift();
+
+        core.submit(
+            gameData, decision.player, decision.period,
+            decision.price,
+            decision.prod,
+            decision.mk,
+            decision.ci,
+            decision.rd,
+            function (gameData) {
+                // accepted
+                decision.callback(gameData);
+                handleDecisions(storage, gameData, callback, fail);
+            },
+            function (gameData) {
+                // declined
+                decision.fail(gameData);
+                handleDecisions(storage, gameData, callback, fail);
+            }
+        );
+    } else {
+        // close
+
+        core.close(gameData, callback, fail); // exit recursion
+    }
+};
+
+var storeData = function (storage, gameData, oldData, snapshot, callback) {
+    if (!gameData || gameData.length != oldData.length) {
+        throw Error('data broken');
+    }
+
+    // generate an unique id (assumed unique)
+    var uid = Number(new Date());
+
+    // store data
+    storage.staticSet(
+        'data', gameData,
+        function (doc) {
+            storage.staticSet(
+                'uid', uid,
+                callback
+            );
+        }
+    );
+
+    if (snapshot) {
+        // store snapshot
+        storage.staticSet(
+            'data_' + uid, gameData,
+            function (doc) {
+                // nothing
+            }
+        );
+    }
+};
+
 var action = function (storage) {
     if (storage.dynamicGet('active')) {
         return;
@@ -27,96 +103,23 @@ var action = function (storage) {
 
         storage.dynamicSet('active', true);
 
-        var handleDecision = function (gameData) {
-            var oldData = gameData;
-
-            var decisions = storage.dynamicGet('decisions');
-
-            if (decisions.length > 0) {
-                // submit
-
-                // get decision from queue
-                var decision = decisions.shift();
-
-                core.submit(
-                    gameData, decision.player, decision.period,
-                    decision.price,
-                    decision.prod,
-                    decision.mk,
-                    decision.ci,
-                    decision.rd,
-                    function (gameData) {
-                        // accepted
-                        decision.callback(gameData);
-                        handleDecision(gameData);
-                    },
-                    function (gameData) {
-                        // declined
-                        decision.fail(gameData);
-                        handleDecision(gameData);
-                    }
-                );
-            } else {
-                // close
-
-                // generate an unique id (assumed unique)
-                var uid = Number(new Date());
-
-                core.close(
-                    gameData,
-                    function (gameData) {
-                        // closed
-
-                        if (!gameData || gameData.length != oldData.length) {
-                            throw Error('data broken');
-                        }
-
-                        // store data
-                        storage.staticSet(
-                            'data', gameData,
-                            function (doc) {
-                                storage.staticSet(
-                                    'uid', uid,
-                                    function (doc) {
-                                        storage.dynamicSet('active', false);
-                                    }
-                                );
-                            }
-                        );
-
-                        // store snapshot
-                        storage.staticSet(
-                            'data_' + uid, gameData,
-                            function (doc) {}
-                        );
-                    },
-                    function (gameData) {
-                        // not closed
-
-                        if (!gameData || gameData.length != oldData.length) {
-                            throw Error('data broken');
-                        }
-
-                        // store data
-                        storage.staticSet(
-                            'data', gameData,
-                            function (doc) {
-                                storage.staticSet(
-                                    'uid', uid,
-                                    function (doc) {
-                                        storage.dynamicSet('active', false);
-                                    }
-                                );
-                            }
-                        );
-                    }
-                );
-            }
-        };
-
         // get data and do handling
         storage.staticGet('data', function (dataObj) {
-            handleDecision(dataObj.buffer);
+            var oldData = dataObj.buffer;
+
+            handleDecisions(
+                storage, oldData,
+                function (gameData) {
+                    storeData(storage, gameData, oldData, true, function () {
+                        storage.dynamicSet('active', false);
+                    });
+                },
+                function (gameData) {
+                    storeData(storage, gameData, oldData, false, function () {
+                        storage.dynamicSet('active', false);
+                    });
+                }
+            );
         });
     });
 };
@@ -126,26 +129,20 @@ module.exports.submit = function (
     price, prod, mk, ci, rd,
     callback, fail
 ) {
-    var decisions = storage.dynamicGet('decisions');
-
-    if (typeof decisions != 'array') {
-        decisions = [];
-
-        storage.dynamicSet('decisions', decisions);
-    }
-
-    // insert into queue
-    decisions.push({
-        player: player,
-        period: period,
-        price: price,
-        prod: prod,
-        mk: mk,
-        ci: ci,
-        rd: rd,
-        callback: callback,
-        fail: fail,
-    });
+    addDecision(
+        storage,
+        {
+            player: player,
+            period: period,
+            price: price,
+            prod: prod,
+            mk: mk,
+            ci: ci,
+            rd: rd,
+            callback: callback,
+            fail: fail,
+        }
+    );
 
     action(storage);
 };

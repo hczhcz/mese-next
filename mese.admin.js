060d18f5295bb8d1a24043b14b1ee9a43ad7d7e5
hcz <h.cz@qq.com>
2016-02-20 18:22:59 -0500

    working

--- mese.admin.js
+++ mese.admin.js
@@ -2,11 +2,35 @@
 
 var core = require('./mese.core');
 
+// settings: [{key: value, ...}, ...]
+var allocator = function (settings, callback) {
+    return function (gameData) {
+        module.exports.alloc(gameData, settings, callback);
+    };
+};
+
 module.exports.init = function (
-    players, settings,
+    count, preset, settings,
     callback
 ) {
-    // TODO
+    core.init(
+        count, preset, settings[0],
+        allocator(settings.slice(1), callback)
+    );
+};
+
+module.exports.alloc = function (
+    gameData, settings,
+    callback
+) {
+    if (settings.length > 0) {
+        core.alloc(
+            gameData, settings[0],
+            allocator(settings.slice(1), callback)
+        )
+    } else {
+        callback(gameData);
+    }
 };
 
 module.exports.print = core.printFull;

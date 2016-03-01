264f7d6f701abf099d9a6805bc6e58442fa93cd0
hcz <h.cz@qq.com>
2016-03-01 00:24:24 -0500

    modify

--- mese.config.js
+++ mese.config.js
@@ -1,9 +1,11 @@
 'use strict';
 
-module.exports.db = 'mongodb://localhost/mese';
-module.exports.port = 63000;
 module.exports.core = './mese';
 module.exports.coreMaxPlayer = 32;
 module.exports.coreMinDataSize = 4096; // for db data protection
+
+module.exports.db = 'mongodb://localhost/mese';
+module.exports.port = 63000;
+
 module.exports.adminName = 'admin';
 module.exports.adminPassword = 'echopen';

3d591fcf880e477224773562cf9c983410f1562e
hcz <h.cz@qq.com>
2016-03-01 00:25:57 -0500

    use express framework

--- mese.js
+++ mese.js
@@ -1,13 +1,14 @@
 'use strict';
 
 var http = require('http'); // TODO: https?
+var express = require('express');
+var compression = require('compression');
 var io = require('socket.io');
 
 var config = require('./mese.config');
 var util = require('./mese.util');
 var db = require('./mese.db');
 var socket = require('./mese.socket');
-var web = require('./mese.web');
 
 process.on('uncaughtException', function (err) {
     util.log('uncaught exception');
@@ -19,9 +20,35 @@ util.log('db init ' + config.db);
 db.init(config.db, function () {
     util.log('server init ' + config.port);
 
-    var server = http.createServer(web).listen(config.port);
+    var app = express();
+    var server = http.createServer(app);
 
-    util.log('socket init');
+    app.use(function (req, res, next) {
+        util.log('web ' + req.ip + ' ' + req.url);
+        next();
+    });
+
+    app.use(compression());
+    app.use(express.static('./res', {
+        index: 'page.html',
+        fallthrough: false,
+    }));
+
+    app.use(function (err, req, res, next) {
+        if (err.status == 404) {
+            util.log('web not found ' + req.path);
+        } else {
+            util.log('web error');
+            util.log(err.stack || err);
+        }
+
+        res.status(err.status || 500);
+        res.end();
+    });
 
     io(server).on('connection', socket);
+
+    server.listen(config.port, function () {
+        util.log('server ready');
+    });
 });

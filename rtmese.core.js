87849b3eeb6b7d1d94a1749d04d8a004a3885841
hcz <h.cz@qq.com>
2016-06-20 17:37:10 +0800

    modify the structure of game data

--- rtmese.core.js
+++ rtmese.core.js
@@ -2,6 +2,8 @@
 
 var engine = require('./rtmese.engine');
 
+// events: {tick: {key: value, ...}, ...}
+// submissions: {tick: {player: [...]}, ...}
 module.exports.init = function (count, delta, events) {
     var game = engine.init(count);
 
@@ -16,27 +18,27 @@ module.exports.submit = function (
     price, prod_rate, mk, ci, rd
 ) {
     if (!game.submissions[game.now_tick]) {
-        game.submissions[game.now_tick] = [];
+        game.submissions[game.now_tick] = {};
     }
 
-    game.submissions[game.now_tick].push([
-        player, price, prod_rate, mk, ci, rd
+    game.submissions[game.now_tick][player] = [
+        price, prod_rate, mk, ci, rd
     ]);
 };
 
 module.exports.exec = function (game) {
-    var events = game.events[game.now_tick] || [];
+    var events = game.events[game.now_tick] || {};
     for (var i in events) {
-        game.settings[events[i][0]] = events[i][1];
+        game.settings[i] = events[i];
     }
 
-    var submissions = game.submissions[game.now_tick] || [];
+    var submissions = game.submissions[game.now_tick] || {};
     for (var i in submissions) {
-        game.decisions.price[submissions[i][0]] = submissions[i][1];
-        game.decisions.prod_rate[submissions[i][0]] = submissions[i][2];
-        game.decisions.mk[submissions[i][0]] = submissions[i][3];
-        game.decisions.ci[submissions[i][0]] = submissions[i][4];
-        game.decisions.rd[submissions[i][0]] = submissions[i][5];
+        game.decisions.price[i] = submissions[i][0];
+        game.decisions.prod_rate[i] = submissions[i][1];
+        game.decisions.mk[i] = submissions[i][2];
+        game.decisions.ci[i] = submissions[i][3];
+        game.decisions.rd[i] = submissions[i][4];
     }
 
     game.now_tick += 1;

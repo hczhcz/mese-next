bb34774dbcd6ab7d3980cb40df974c74f469ebf4
hcz <h.cz@qq.com>
2016-06-24 20:43:09 +0800

    modify

--- web/ui.bind.js
+++ web/ui.bind.js
@@ -2,8 +2,8 @@
 
 define('ui.bind', function (require, module) {
     module.exports.variable = function (key, value) {
-        $('td[var=' + key + ']').text(value);
-        $('span[var=' + key + ']').text(value);
-        $('input[var=' + key + ']').val(value);
+        $('td[var="' + key + '"]').text(value);
+        $('span[var="' + key + '"]').text(value);
+        $('input[var="' + key + '"]').val(value);
     };
 });
